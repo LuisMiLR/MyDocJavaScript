7f68c9660ee7b7a0887b1435c9d2261489cc240f
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-02-03 23:52:04 +0100

    add folder numbers & file remainder operator

---
+++ .history/numbers/remainderOperator_20240203235056.js
@@ -0,0 +1,17 @@
+//remainderOperator
+
+/*l'opérateur Reste ( %). Lorsque vous obtenez le reste de la division d'un nombre par deux, vous obtiendrez soit 0ou 1. Cela vous aidera à savoir si le nombre initial est pair (divisible par deux sans reste) ou impair .
+
+Jetons un coup d'oeil à quelques chiffres : */
+
+// even numbers
+4 % 2; // 0
+6 % 2; // 0
+8 % 2; // 0
+10 % 2; // 0
+
+// odd numbers
+3 % 2; // 1
+5 % 2; // 1
+7 % 2; // 1
+9 % 2; // 1
