e8ab164804a2d28970944b4a6461376f124eb646
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-02-07 23:26:28 +0100

    add info and folder courses

---
+++ loops/intro.js
@@ -0,0 +1,18 @@
+/* Definition 
+Une boucle en programmation est une structure de contrôle permettant d'exécuter une séquence d'instructions de manière répétée tant qu'une condition spécifique est vraie, ou jusqu'à ce qu'une condition soit remplie.
+
+Les boucles sont un élément fondamental de la programmation, et elles permettent aux développeurs d'automatiser des tâches répétitives. En JavaScript, comme dans de nombreux autres langages de programmation, deux des types de boucles les plus couramment utilisés sont la boucle `while` et la boucle `for`.
+
+- **Boucle `while` :** Elle exécute un bloc d'instructions tant qu'une condition spécifiée reste vraie. La syntaxe est la suivante :
+  ```javascript */
+while (condition) {
+  // instructions à exécuter tant que la condition est vraie
+}
+
+/*- **Boucle `for` :** Elle offre une structure plus compacte, regroupant souvent l'initialisation, la condition et l'itération dans une seule ligne. La syntaxe est la suivante :
+  ```javascript */
+for (initialisation; condition; itération) {
+  // instructions à exécuter tant que la condition est vraie
+}
+
+//Ces deux types de boucles sont largement utilisés dans JavaScript et offrent aux programmeurs la flexibilité nécessaire pour effectuer des itérations sur des séquences de données, répéter des blocs de code, et gérer des opérations répétitives. */
