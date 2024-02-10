48a1490fb7f2edc32a2bcb7cb95b840d00501263
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-02-10 23:13:50 +0100

    more array methode exemple

---
+++ array/filter.js
@@ -0,0 +1,68 @@
+// Array filter
+
+/* Nous explorerons dans ce chapitre davantage de méthodes de tableau tout en découvrant simultanément les rappels.
+
+Une méthode d'un tableau commun est la méthode .filter() 
+Lorsque vous appelez cette méthode sur un tableau, vous obtiendrez un autre tableau 
+contenant certains éléments du tableau d'origine, en fonction de la condition que vous spécifiez. Prenons un exemple : */
+const numbers = [9, 5, 14, 3, 11];
+
+const numbersAboveTen = numbers.filter(function (number) {
+  return number > 10;
+});
+console.log(numbersAboveTen); // [14, 11]
+///Ne pas oublier pas le mot-clé return à l'intérieur de la fonction de rappel .
+/* 
+
+
+Array.filter (rappel)
+Voyons comment fonctionne le code ci-dessus en décomposant son exécution étape par étape.
+
+La .filter()méthode attend un rappel comme premier argument. Dans notre exemple, le rappel est : */
+
+//function(number) {
+//return number > 10;
+//}
+
+/*JavaScript prendra votre rappel et l'appellera pour chaque élément du tableau. Notre numberstableau contient 5 éléments, 
+il l'appellera donc 5 fois. Chaque fois qu'il appelle cette fonction, il donnera une valeur au numberparamètre que vous avez spécifié dans ce rappel.
+
+-Lors de sa première exécution, il donnera la numbervaleur 9 (le premier élément du tableau).
+-La deuxième fois qu'il s'exécutera, il donnera la numbervaleur 5 (le deuxième élément du tableau).
+et ainsi de suite jusqu'au dernier élément du tableau.
+
+C'est ainsi que fonctionnent les rappels. Désormais, chaque méthode tableau a un comportement différent 
+que nous allons expliquer. Ce comportement dépend souvent du résultat du rappel!!
+
+Dans cet exemple, si la fonction de rappel renvoie true, alors l'élément sera inclus dans le tableau final renvoyé par .filter(). 
+Cependant, si la fonction de rappel renvoie false, alors l'élément ne sera pas inclus dans le tableau final.
+
+Cela signifie que si vous avez le code ci-dessous :
+*/
+numbers.filter(function (number) {
+  return true;
+});
+
+/* Cela renverra chaque élément du tableau. Vous finirez donc par obtenir une copie du tableau d'origine. 
+C'est parce que le rappel renvoie toujours vrai. Ce code n'est pas très utile, mais il sert à vous montrer 
+l'importance de ce que renvoie la fonction de rappel et comment cela affecte le résultat de la .filter()méthode.
+
+C'est pourquoi nous avions une condition number > 10. Cette condition renverra soit true ou false en fonction de la valeur du number. 
+
+Comment JavaScript sait-il que chaque élément du numberstableau devient numberl'argument de rappel ? 
+Et la réponse à cette question est qu’il ne sait pas !
+
+JavaScript prendra votre rappel et transmettra l'élément du tableau comme premier paramètre à votre fonction de rappel. 
+Cela signifie que le code ci-dessous fonctionne (mais n'est pas recommandé) :
+
+
+résumer
+-La .filter()méthode renvoie un nouveau tableau contenant certains éléments du tableau d'origine, en fonction de la condition 
+que vous spécifiez.
+-JavaScript prendra votre fonction de rappel et l'appellera pour chaque élément du tableau.
+-Pour la .filter()méthode, le résultat de la fonction de rappel compte. Quand c'est le cas true, l'élément sera inclus dans le 
+tableau résultant. Sinon, ce ne sera pas le cas.
+-JavaScript ne peut pas deviner intelligemment que le numberstableau devient le numberparamètre de votre fonction de rappel. 
+Ce qu'il fait, c'est qu'il appelle votre fonction de rappel tout en donnant une valeur pour le premier paramètre que vous avez spécifié.
+-Utilisez la convention de dénomination pluriel -> singulier lorsque vous utilisez la .filter()méthode.
+*/
---
+++ array/moreArrayMethode.js
@@ -0,0 +1,75 @@
+/* More array methode 
+
+///////////// Array map
+
+
+La .map(callback)méthode permet de transformer un tableau en un autre. 
+
+
+[4, 2, 5, 8]transformé en [8, 4, 10, 16]. Nous avons doublé chaque élément du tableau d'origine.
+["sam", "Alex"]transformé en ["SAM", "ALEX"]. Nous avons mis en majuscule chaque élément du tableau d'origine.
+
+Notez que vous récupérez toujours un tableau contenant le même nombre d'éléments par rapport au tableau d'origine, 
+mais chaque élément a probablement subi une transformation .
+
+Dans le premier exemple, la transformation consiste à multiplier chaque nombre par 2.
+Dans le deuxième exemple, la transformation consiste à faire appel .toUpperCase()à chaque élément.
+
+Voyons comment nous pouvons mettre en œuvre ces transformations :
+*/
+
+const numbers = [4, 2, 5, 8];
+
+const doubled = numbers.map(function (number) {
+  return number * 2;
+});
+console.log(doubled); // [8, 4, 10, 16]
+
+//et
+
+const names = ["sam", "Alex"];
+const upperNames = names.map(function (name) {
+  return name.toUpperCase();
+});
+
+/* Si vous oubliez de le faire returndepuis la fonction de rappel, vous vous retrouverez avec le tableau suivant : 
+[undefined, undefined]. En effet, pour chaque élément du tableau d'origine ( ["sam", "Alex"]), vous revenez undefined 
+donc le résultat final sera [undefined, undefined].
+
+Une fois que vous faites cette erreur plusieurs fois, cela devient un signal clair que vous avez oublié le mot-clé return. 
+
+
+//////////////////////////Array includes(item)
+
+La .includes(item)méthode tableau est l'une des méthodes tableau les plus simples car elle nécessite un itemrappel plutôt qu'un rappel 
+et renvoie true lorsqu'il item existe dans le tableau et false autrement. Voici un exemple :
+*/
+
+const groceries = ["Apple", "Peach", "Tomato"];
+
+groceries.includes("Tomato"); // true
+groceries.includes("Bread"); // false
+
+/*  Array join(glue) jointure de tableau colle 
+
+Lorsque vous avez un tableau et que vous affichez ce tableau sur une page Web (comme nous le verrons plus tard dans la section DOM 
+du cours), le tableau sera automatiquement converti en chaîne. JavaScript invoquera automatiquement la .toString()méthode du tableau 
+qui renvoie une chaîne d'éléments du tableau séparés par des virgules. Voici comment cela fonctionne:
+*/
+const groceriess = ["Apple", "Peach", "Tomato"];
+groceriess.toString(); // "Apple,Peach,Tomato"
+
+/* Mais il y a un inconvénient, c'est que vous ne pouvez pas personnaliser la colle qui est insérée entre les éléments du tableau, 
+qui est la virgule ,. 
+
+Si vous souhaitez personnaliser la colle, vous pouvez utiliser la .join(glue)méthode :
+*/
+
+const groceriez = ["Apple", "Peach", "Tomato"];
+groceriez.join("; "); // "Apple; Peach; Tomato"
+groceriez.join(" . "); // "Apple . Peach . Tomato"
+
+/* résumer
+La méthode array .map(callback)permet de transformer un tableau en un autre.
+.includes(item)La méthode tableau prend un itemet renvoie truelorsqu'il itemexiste dans le tableau et falseautrement.
+La méthode array .join(glue)renvoie une chaîne d'éléments du tableau séparés par le glue. */
