0c91e600f83cbe647bac6953600d13171dbc5da0
LuisMiLR <luismiguel.robles.lr@gmail.com>
2024-03-12 19:46:00 +0100

    new page definition

---
+++ defenition-words.js
@@ -0,0 +1,37 @@
+/* qu'est ce qu'une référence ?
+
+En programmation, une "référence" fait référence à une valeur qui permet d'accéder à une ressource ou à un objet en mémoire. En d'autres termes, une référence 
+est une manière d'indiquer l'emplacement en mémoire d'un objet ou d'une variable plutôt que de stocker directement sa valeur.
+Voici quelques points clés :
+
+Accès à la mémoire : Une référence est utilisée pour accéder à l'emplacement en mémoire où une valeur est stockée. Au lieu de stocker la valeur 
+elle-même, une variable peut contenir une référence qui pointe vers l'emplacement mémoire de la valeur.
+
+Objets et structures de données complexes : Les références sont souvent utilisées avec des objets ou des structures de données complexes. Lorsque vous 
+travaillez avec des objets, des tableaux, des fonctions, etc., une référence est souvent utilisée pour accéder à ces entités en mémoire.
+
+Affectation de variables : Lorsque vous affectez une variable à une autre, la référence à l'objet ou à la valeur est copiée, pas la valeur elle-même. 
+Cela signifie que les deux variables peuvent faire référence au même emplacement mémoire.
+*/
+
+/* qu'est ce qu'une classe ? 
+Classe : Une classe est une sorte de modèle ou de plan qui décrit comment créer un objet. Elle spécifie les propriétés (variables) et les méthodes (fonctions) que 
+les objets créés à partir de cette classe auront. */
+
+/*
+ qu est ce qu'une instance ?
+
+En programmation, une "instance" se réfère à un objet spécifique créé à partir d'une classe ou d'un constructeur. Une classe est un modèle ou un plan qui 
+définit la structure et le comportement d'objets, et une instance est un objet concret qui est une occurrence de cette classe.
+
+Voici une explication plus détaillée :
+Instance : Une instance est un objet spécifique qui est créé à partir d'une classe. Chaque instance a ses propres valeurs pour les propriétés définies dans 
+la classe, mais partage les méthodes définies dans la classe.
+*/
+
+/*
+cors c'est quoi ?
+
+CORS, ou Cross-Origin Resource Sharing, est un mécanisme qui permet aux navigateurs web d'autoriser les requêtes HTTP entre des domaines différents, renforçant 
+ainsi la sécurité tout en facilitant l'accès à des ressources distantes.
+*/
